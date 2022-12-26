449ca77c492f0f0337556d455407ca9f6f3b9a7b
Rony <ronyjohn9870@gmail.com>
2022-12-26 19:35:14 +0200

    Some Changes

--- src/Components/bookDetails.js
+++ src/Components/bookDetails.js
@@ -1,52 +1,47 @@
 import * as React from "react";
+import "../App.css";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 export default function BookDetails({ Item, Showing, onClose }) {
-  //   const [open, setOpen] = React.useState(Showing);
-  //   const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
   // console.log("book", Item.volumeInfo);
-  // if (Item !== undefined) {
-  // let Author = Item.volumeInfo.authors;
-  // let Title = ;
-  // let Publisher = Item.volumeInfo.publisher;
-  // let Language = Item.volumeInfo.language;
-  // let PageCount = Item.volumeInfo.pageCount;
-  // let epub = Item.accessInfo.epub.downloadLink;
-  // }
   return (
     <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <Modal
-        open={Showing}
-        onClose={onClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Authors
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {/* {Author} */}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {/* $$ {Publisher} */}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {/* {Language} */}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {/* {PageCount} */}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {/* {epub} */}
-          </Typography>
-        </Box>
-      </Modal>
+      {Item === undefined ? (
+        <> </>
+      ) : (
+        <Modal
+          open={Showing}
+          onClose={onClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Title: {Item.volumeInfo.title}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Author(s): {Item.volumeInfo.authors.join(" & ")}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {Item.volumeInfo.publisher &&
+                `Publisher: ${Item.volumeInfo.publisher}`}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              language: {Item.volumeInfo.language}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Pages: {Item.volumeInfo.pageCount}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Free Download Link:
+              <br />
+              {Item.accessInfo.epub.downloadLink}`
+            </Typography>
+          </Box>
+        </Modal>
+      )}
     </div>
   );
 }
@@ -56,9 +51,9 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 850,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  border: "2px solid Red",
   boxShadow: 24,
-  p: 4,
+  p: 8,
 };
